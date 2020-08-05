840d0944b68e6fc6db496a0f4dc711b21f5b339c
David <davi5663@edu.sde.dk>
2020-08-05 11:08:41 +0200

    Added libaries

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "mbed.h"
+#include "Grove_LCD_RGB_Backlight.h" //Libary for LCD Backligt
+#include "stm32746g_discovery_lcd.h" //Libary for TouchScreen
 
 // main() runs in its own thread in the OS
 int main()
