3efcf01dec41f85816dc214065efbce24a1435e8
David <davi5663@edu.sde.dk>
2020-08-05 12:35:53 +0200

    Added a text

--- main.cpp
+++ main.cpp
@@ -12,8 +12,12 @@ int main() {
   BSP_LCD_Init();
   BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
   BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+  BSP_LCD_Clear(LCD_COLOR_WHITE);
   BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
-  BSP_LCD_DisplayStringAt(0, 0, (uint8_t *)"TEST",CENTER_MODE); // The first number 0 goes more to the right direction
+  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+  BSP_LCD_DisplayStringAt(
+      0, 0, (uint8_t *)"TEST",
+      CENTER_MODE); // The first number 0 goes more to the right direction
 
   while (true) {
   }
