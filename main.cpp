dd8205a47673c8d6be0e69f23dbca6c6bc45f7f3
David <davi5663@edu.sde.dk>
2020-08-06 07:42:16 +0200

    Changed humidity

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include "mbed.h"
 #include "DHT.h"                     //Libary for Tempature & Humidity
 #include "Grove_LCD_RGB_Backlight.h" //Libary for LCD Backligt
+#include "mbed.h"
 #include "stm32746g_discovery_lcd.h" //Libary for TouchScreen
 #include <string>
 
@@ -8,7 +8,7 @@ DigitalOut blueled(D3);
 DigitalOut redled(D2);
 DigitalIn button(D4);
 DigitalOut buzzer(D8);
-DHT Sensor(D7, SEN51035P);
+DHT Sensor(D6, DHT22);
 
 int err;
 
@@ -16,8 +16,8 @@ string office = "Office 345- David";
 
 // main() runs in its own thread in the OS
 int main() {
+  float f = 0.0f;
 
-    err = Sensor.readData();
   BSP_LCD_Init();
   BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
   BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
@@ -27,13 +27,17 @@ int main() {
   BSP_LCD_DisplayStringAt(
       0, 0, (uint8_t *)"TEST",
       CENTER_MODE); // The first number 0 goes more to the right direction
-
+  printf("New Test version 2:\n\r");
   while (1) {
-      if (0 == err){
-          printf("Temp: %4.2f \n \r", Sensor.ReadTemperature(CELCIUS));
-      }
-      else{
-          
-      }
+    err = Sensor.readData();
+    if (0 == err) {
+      f = Sensor.ReadTemperature(FARENHEIT);
+      printf("Temp: F %4.2f", f);
+      f = Sensor.ReadTemperature(CELCIUS);
+      printf(", C %4.2f\n \r", f);
+    } else {
+      printf("Error code %i", err);
+    }
+    wait_us(1000000);
   }
 }
