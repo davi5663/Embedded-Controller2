0fc19926721164e5fe2623d6ebf1013a4edf669f
David <davi5663@edu.sde.dk>
2020-08-06 09:37:19 +0200

    Added Tempature & Humidity on touchscreen

--- main.cpp
+++ main.cpp
@@ -12,11 +12,12 @@ DHT Sensor(D6, DHT22);
 
 int err;
 
-string office = "Office 345- David";
+
 
 // main() runs in its own thread in the OS
 int main() {
   float f = 0.0f;
+  
 
   BSP_LCD_Init();
   BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
@@ -24,19 +25,27 @@ int main() {
   BSP_LCD_Clear(LCD_COLOR_WHITE);
   BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
   BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-  BSP_LCD_DisplayStringAt(
-      0, 0, (uint8_t *)"TEST",
-      CENTER_MODE); // The first number 0 goes more to the right direction
+  char text[25];
+  //string office = "Office 345- David";
+  sprintf(text, "Office %i", int(text));
+  BSP_LCD_DisplayStringAt(0, 180, (uint8_t *)text, CENTER_MODE); // The first number 0 goes more to the right direction
+      
   printf("New Test version 2:\n\r");
   while (1) {
     err = Sensor.readData();
+    f = Sensor.ReadTemperature(CELCIUS);
+    char tempature[25];
+    sprintf(tempature, "Temprature %i", int(f));
+    BSP_LCD_DisplayStringAt(0,0, (uint8_t *)tempature, CENTER_MODE );
+
     if (0 == err) {
       f = Sensor.ReadTemperature(FARENHEIT);
       printf("Temp: F %4.2f", f);
       f = Sensor.ReadTemperature(CELCIUS);
       printf(", C %4.2f\n \r", f);
-    } else {
-      printf("Error code %i", err);
+      f = Sensor.ReadHumidity();
+      printf("Humidty: H %4.2f\n \r ", f);
+      
     }
     wait_us(1000000);
   }
